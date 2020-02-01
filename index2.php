5ab6e9ce757d89f50e9d18a23374d98c8aca87a6
shankar124 <shankarmishra124@gmail.com>
2020-02-01 12:46:42 +0530

    create index2.php

---
+++ index2.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "test-db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT user_id, first_name, last_name FROM user_details";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br> id: ". $row["user_id"]. " - Name: ". $row["first_name"]. " " . $row["last_name"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
+
+</body>
+</html>
