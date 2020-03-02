7c3b025a4d934e7520bfd3878e46be8e6c7d6a9f
shankar124 <shankarmishra124@gmail.com>
2020-03-02 13:28:54 +0530

    Delete dbversion1.sql

--- dbversion1.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `CouponLog` ADD `CouponID` INT(11) NOT NULL AFTER `ConfirmID`;
-ALTER TABLE `CouponLog` ADD `Percentage` TINYINT(2) NOT NULL AFTER `Coupon`;
-
-ALTER TABLE `CouponLog` CHANGE `CouponID` `CouponID` INT(11) NULL DEFAULT NULL;
-ALTER TABLE `CouponLog` CHANGE `Percentage` `Percentage` TINYINT(2) NOT NULL DEFAULT '0';
-
-
-INSERT INTO `version` (`dbVersion`) VALUES ('1');
